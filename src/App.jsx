2f045d83b068294f6dba8eeef7ad92a7e31ea1cc
imadbenmadi <imadbenmadi891@gmail.com>
2025-02-10 13:25:35 +0100

    remove commented-out authentication and font fetching logic in App component

--- src/App.jsx
+++ src/App.jsx
@@ -10,64 +10,65 @@ function App() {
     const [loading, setLoading] = useState(true);
     // const [userType, setUserType] = useState(null);
     const { set_Auth, store_login } = useAppContext();
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await axios.get(
-                    "http://localhost:3000/Check_Auth",
-                    {
-                        withCredentials: true,
-                        validateStatus: () => true,
-                    }
-                );
-                console.log("response from check auth", response);
+    // useEffect(() => {
+    //     setLoading(true);
+    //     const fetchData = async () => {
+    //         try {
+    //             const response = await axios.get(
+    //                 "http://localhost:3000/Check_Auth",
+    //                 {
+    //                     withCredentials: true,
+    //                     validateStatus: () => true,
+    //                 }
+    //             );
+    //             console.log("response from check auth", response);
 
-                if (response.status == 200) {
-                    // store_login(response.data.userId, response.data.userType);
-                    // setUserType(response.data.userType);
-                    if (!response.data.userType || !response.data.userId) {
-                        set_Auth(false);
-                        return;
-                    }
-                    set_Auth(true);
-                    store_login(response.data.userId, response.data.userType);
-                } else {
-                    set_Auth(false);
-                }
-            } catch (error) {
-                set_Auth(false);
-            }
-        };
+    //             if (response.status == 200) {
+    //                 // store_login(response.data.userId, response.data.userType);
+    //                 // setUserType(response.data.userType);
+    //                 if (!response.data.userType || !response.data.userId) {
+    //                     set_Auth(false);
+    //                     return;
+    //                 }
+    //                 set_Auth(true);
+    //                 store_login(response.data.userId, response.data.userType);
+    //             } else {
+    //                 set_Auth(false);
+    //             }
+    //         } catch (error) {
+    //             set_Auth(false);
+    //         }
+    //     };
 
-        const fetch_fonts = () => {
-            return new Promise((resolve) => {
-                const fontURL =
-                    "https://fonts.googleapis.com/css2?family=Poppins:wght@100..900&display=swap";
-                const link = document.createElement("link");
-                link.href = fontURL;
-                link.rel = "stylesheet";
-                link.onload = () => {
-                    document.getElementById("root").style.fontFamily =
-                        "Poppins, sans-serif";
-                    resolve();
-                };
-                link.onerror = () => {
-                    document.getElementById("root").style.fontFamily =
-                        "sans-serif";
-                    resolve();
-                };
-                document.head.appendChild(link);
-            });
-        };
+    //     const fetch_fonts = () => {
+    //         return new Promise((resolve) => {
+    //             const fontURL =
+    //                 "https://fonts.googleapis.com/css2?family=Poppins:wght@100..900&display=swap";
+    //             const link = document.createElement("link");
+    //             link.href = fontURL;
+    //             link.rel = "stylesheet";
+    //             link.onload = () => {
+    //                 document.getElementById("root").style.fontFamily =
+    //                     "Poppins, sans-serif";
+    //                 resolve();
+    //             };
+    //             link.onerror = () => {
+    //                 document.getElementById("root").style.fontFamily =
+    //                     "sans-serif";
+    //                 resolve();
+    //             };
+    //             document.head.appendChild(link);
+    //         });
+    //     };
 
-        Promise.all([fetch_fonts(), fetchData()])
-            .then(() => {
-                setLoading(false);
-            })
-            .catch(() => {
-                setLoading(false);
-            });
-    }, []);
+    //     Promise.all([fetch_fonts(), fetchData()])
+    //         .then(() => {
+    //             setLoading(false);
+    //         })
+    //         .catch(() => {
+    //             setLoading(false);
+    //         });
+    // }, []);
     if (loading) {
         return (
             <div className=" w-screen h-screen flex flex-col items-center justify-center">
