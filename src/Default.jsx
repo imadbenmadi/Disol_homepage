67c4213c425c113d07d1ebc03165a5cb2e6d7eaa
imadbenmadi <imadbenmadi891@gmail.com>
2025-02-10 13:13:40 +0100

    simplify Default component by removing authentication checks and directly navigating to Home

--- src/Default.jsx
+++ src/Default.jsx
@@ -3,15 +3,23 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router";
 import { useAppContext } from "./AppContext";
 
+// function Default() {
+//     const { isAuth, userType, userId } = useAppContext();
+//     const Navigate = useNavigate();
+//     useEffect(() => {
+//         if (isAuth && userType && userId) {
+//             Navigate("/Home");
+//             return;
+//         }
+//         Navigate("/Login");
+//     }, []);
+// }
 function Default() {
-    const { isAuth, userType, userId } = useAppContext();
     const Navigate = useNavigate();
+
     useEffect(() => {
-        if (isAuth && userType && userId) {
-            Navigate("/Home");
-            return;
-        }
-        Navigate("/Login");
+        Navigate("/Home");
     }, []);
+    return <div>Redirecting...</div>;
 }
 export default Default;
